19619ae079f6a496c898e3444d107f7c0062d6a6
Verbitski A <eternity.2019@mail.ru>
2021-11-17 07:47:54 +0300

    Добалена сущность пользователя и контекст InMemory репозитория

---
+++ Meetins.DAL/EF/InMemoryContext.cs
@@ -0,0 +1,56 @@
+﻿using Meetins.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetins.DAL.EF
+{
+    class InMemoryContext : DbContext
+    {
+        public InMemoryContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=fsapp;Trusted_Connection=True;");
+            optionsBuilder.UseInMemoryDatabase(databaseName: "MeetinsDbInMemory");
+        }
+        public DbSet<UserEntity> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity[]
+                {
+                new UserEntity { 
+                    UserId=new Guid("AC6773D0-17C7-4E94-BBDB-649CD88780C8"), 
+                    FirstName="Пётр",
+                    LastName = "Петров",
+                    PhoneNumber = "+375299998877",
+                    Email = "petrov@gmail.com",
+                    Password = "hash_password_petr",
+                    Gender = "M",
+                    UserIcon = "path_to_petr_avatar.png",
+                    DateRegister = new DateTime(2021-11-14),
+                    RememberMe = true
+                },
+                new UserEntity { 
+                    UserId=new Guid("187AC176-CB28-4456-9AB5-D3A1EF370542"),
+                    FirstName="Жанна",
+                    LastName = "Агузарова",
+                    PhoneNumber = "+375291112233",
+                    Email = "janna@gmail.com",
+                    Password = "janna_hash",
+                    Gender = "F",
+                    UserIcon = "path_to_janna_avatar.png",
+                    DateRegister = new DateTime(2021-11-12),
+                    RememberMe = false
+                }
+                });
+        }
+    }
+}
---
+++ Meetins.DAL/Entities/UserEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetins.DAL.Entities
+{
+    [Table("Users", Schema = "Users")]
+    class UserEntity
+    {
+        [Column("UserId")]
+        public Guid UserId { get; set; }
+
+        [Column("FirstName")]
+        public string FirstName { get; set; }
+
+        [Column("LastName")]
+        public string LastName { get; set; }
+
+        [Column("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("Password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// M - мужчина, F - женщина.
+        /// </summary>
+        [Column("Gender")]
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Путь к иконке пользователя.
+        /// </summary>
+        [Column("UserIcon")]
+        public string UserIcon { get; set; }
+
+        /// <summary>
+        /// Дата регистрации пользователя.
+        /// </summary>
+        [Column("DateRegister")]
+        public DateTime DateRegister { get; set; }
+
+        [Column("RememberMe")]
+        public bool RememberMe { get; set; }
+    }
+}
