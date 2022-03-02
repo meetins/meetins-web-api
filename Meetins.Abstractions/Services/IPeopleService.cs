f2efa5111d7382c0149ba5419fbc89823469fbb0
Yana Latysh <123@123.by>
2022-03-03 01:43:32 +0300

    Получение списка пользователей. Создание GET метода в контроллере. Создание репозитория PeopleRepository, сервиса PeopleService. Создана выходная модель PeopleOutput

---
+++ Meetins.Abstractions/Services/IPeopleService.cs
@@ -0,0 +1,23 @@
+﻿using Meetins.Abstractions.Repositories;
+using Meetins.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetins.Abstractions.Services
+{
+    /// <summary>
+    /// Абстракция всех пользователей.
+    /// </summary>
+    public interface IPeopleService
+    {
+        /// <summary>
+        /// Получение списка всех пользователей.
+        /// </summary>
+        /// <param name="userId"> Идентификатор авторизованного пользователя. </param>
+        /// <returns> Список всех существующих пользователей. </returns>
+        Task<IEnumerable<PeopleOutput>> GetAllPeoplesAsync(Guid userId);
+    }
+}
---
+++ Meetins.Contollers/PeopleController.cs
@@ -0,0 +1,48 @@
+﻿using Meetins.Abstractions.Services;
+using Meetins.Models.People;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meetins.Controllers
+{
+    /// <summary>
+    /// В контроллере содержится функционал для получения списка всех пользователей.
+    /// </summary>
+    [Route("people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private IPeopleService _peopleService;
+        
+        public PeopleController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;            
+        }
+
+        /// <summary>
+        /// Получение списка всех существующих пользователей.
+        /// </summary>
+        /// <returns> Список пользователей. </returns>
+        [Authorize]
+        [HttpGet, Route("all")]
+        public async Task<ActionResult<IEnumerable<PeopleOutput>>> GetAllPeoplesAsync()
+        {
+            string rawUserId = HttpContext.User.FindFirst("userId").Value;
+
+            //string rawUserId = "187ac176-cb28-4456-9ab5-d3a1ef370500";
+
+            if (!Guid.TryParse(rawUserId, out Guid userId))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _peopleService.GetAllPeoplesAsync(userId);
+
+            return Ok(result);
+        }
+    }
+}
